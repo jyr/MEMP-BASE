5abfa8162bbf8f32f387cd3a450260e0d6e889c8
Jair Gaxiola <jyr.gaxiola@gmail.com>
2011-01-16 22:13:49 -0600

    makeKeyAndOrderFront UI preferences

--- memp/PreferencesController.py
+++ memp/PreferencesController.py
@@ -7,14 +7,13 @@
 
 from Foundation import *
 from AppKit import *
+
 import objc
+
 class PreferencesController (NSViewController):	
 	def show(self):
-		#print dir(PreferencesController)
 		self.PreferencesController = PreferencesController.alloc().initWithNibName_bundle_("Preferences", None)
-		#print dir(self.PreferencesController)
-		self.PreferencesController.view()
-		self.PreferencesController.retain()
+		self.PreferencesController.view().window().makeKeyAndOrderFront_(self)
 	show = classmethod(show)
 
 
